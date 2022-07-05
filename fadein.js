f8103dbdf4b53a7707a32ecaeb20a2c4bd864629
Motoshi0711 <motoshi.suzuki.hp@gmail.com>
2022-07-05 16:12:28 +0900

    all

--- fadein.js
+++ fadein.js
@@ -1,52 +1,52 @@
 // 動きのきっかけとなるアニメーションの名前を定義
-function fadeAnime2(){
+function fadeAnime1(){
 
   // ふわっ
-  $('.fadeInTrigger2').each(function(){ //fadeInTriggerというクラス名が
+  $('.fadeInTrigger1').each(function(){ //fadeInTriggerというクラス名が
     var elemPos = $(this).offset().top-50;//要素より、50px上の
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeIn2');// 画面内に入ったらfadeInというクラス名を追記
+    $(this).addClass('fadeIn1');// 画面内に入ったらfadeInというクラス名を追記
     }else{
-    $(this).removeClass('fadeIn2');// 画面外に出たらfadeInというクラス名を外す
+    $(this).removeClass('fadeIn1');// 画面外に出たらfadeInというクラス名を外す
     }
     });
 }
 
-function fadeAnime3(){
+function fadeAnime2(){
 
     // ふわっ
-    $('.fadeInTrigger3').each(function(){ //fadeInTriggerというクラス名が
+    $('.fadeInTrigger2').each(function(){ //fadeInTriggerというクラス名が
       var elemPos = $(this).offset().top-50;//要素より、50px上の
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
       if (scroll >= elemPos - windowHeight){
-      $(this).addClass('fadeIn3');// 画面内に入ったらfadeInというクラス名を追記
+      $(this).addClass('fadeIn2');// 画面内に入ったらfadeInというクラス名を追記
       }else{
-      $(this).removeClass('fadeIn3');// 画面外に出たらfadeInというクラス名を外す
+      $(this).removeClass('fadeIn2');// 画面外に出たらfadeInというクラス名を外す
       }
       });
   }
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function (){
-fadeAnime2();/* アニメーション用の関数を呼ぶ*/
+fadeAnime1();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function(){
-fadeAnime2();/* アニメーション用の関数を呼ぶ*/
+fadeAnime1();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
 
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function (){
-fadeAnime3();/* アニメーション用の関数を呼ぶ*/
+fadeAnime2();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function(){
-fadeAnime3();/* アニメーション用の関数を呼ぶ*/
+fadeAnime2();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
